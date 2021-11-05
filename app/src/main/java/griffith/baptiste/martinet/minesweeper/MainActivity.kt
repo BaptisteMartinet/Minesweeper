7ded466f4910b7d946803bf18579aa0eb1805141
Baptiste Martinet <baptiste.martinet@epitech.eu>
2021-11-05 23:55:41 +0100

    feat: implement reset game modal

--- app/src/main/java/griffith/baptiste/martinet/minesweeper/MainActivity.kt
+++ app/src/main/java/griffith/baptiste/martinet/minesweeper/MainActivity.kt
@@ -1,23 +1,27 @@
 package griffith.baptiste.martinet.minesweeper
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var minesweeperGame: MinesweeperGame
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     val remainingFlags = findViewById<TextView>(R.id.remainingFlags)
-
-    val minesweeperGame = findViewById<MinesweeperGame>(R.id.game)
+    minesweeperGame = findViewById(R.id.game)
     minesweeperGame.remainingFlagsTextView = remainingFlags
     minesweeperGame.updateRemainingFlagsText()
     val resetBtn = findViewById<Button>(R.id.resetBtn)
     resetBtn.setOnClickListener {
-      minesweeperGame.reset()
+      showDialog()
     }
 
     val modeBtn = findViewById<Button>(R.id.modeBtn)
@@ -30,5 +34,21 @@ class MainActivity : AppCompatActivity() {
         minesweeperGame.setMode(MinesweeperGame.ModeEnum.UNCOVERING)
       }
     }
+  }
+
+  private fun showDialog() {
+    lateinit var dialog: AlertDialog
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle("Reset board?")
+    builder.setMessage("Do you want to reset the game?")
+    val dialogClickListener = DialogInterface.OnClickListener { _, which ->
+      when (which) {
+        DialogInterface.BUTTON_POSITIVE -> minesweeperGame.reset()
+      }
+    }
+    builder.setPositiveButton("YES", dialogClickListener)
+    builder.setNegativeButton("CANCEL", dialogClickListener)
+    dialog = builder.create()
+    dialog.show()
   }
 }
